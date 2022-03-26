84c3ba2a83dfa7506e89862a31281ac06d5df792
andriferry <ferryandri35@gmail.com>
2022-03-26 09:06:49 +0700

    Update scrollbar

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,47 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="red accent-3"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="./assets/logo.png"
-          transition="scale-transition"
-          width="40"
-        />
-      </div>
+    <v-app>
+        <v-app-bar app color="red accent-3" dark>
+            <div class="d-flex align-center">
+                <v-img
+                    alt="Vuetify Logo"
+                    class="shrink mr-2"
+                    contain
+                    src="./assets/logo.png"
+                    transition="scale-transition"
+                    width="40"
+                />
+            </div>
 
-      <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+            <v-btn
+                href="https://github.com/vuetifyjs/vuetify/releases/latest"
+                target="_blank"
+                text
+            >
+                <span class="mr-2">Latest Release</span>
+                <v-icon>mdi-open-in-new</v-icon>
+            </v-btn>
+        </v-app-bar>
 
-    <v-main class="red accent-2">
-      <router-view/>
-    </v-main>
-  </v-app>
+        <v-main class="red accent-2 mainStyle">
+            <router-view />
+        </v-main>
+    </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
+    name: 'App',
 };
 </script>
+
+<style scoped>
+.mainStyle {
+    overflow-x: auto;
+    scrollbar-color: #ff1744 #ff5252;
+    -webkit-overflow-scrolling: touch;
+    -webkit-appearance: none;
+}
+
+</style>
--- src/components/HomePages.vue
+++ src/components/HomePages.vue
@@ -1,13 +1,36 @@
 <template>
-  <v-container>
-    <p class="mb-0 text-h1 black--text">
-      Hello
-    </p>
-  </v-container>
+    <v-container fluid>
+        <div class="d-flex" style="gap: 1rem;">
+            <v-card v-for="data in 15" :key="data"  max-width="344" outlined>
+                <v-list-item three-line>
+                    <v-list-item-content>
+                        <div class="text-overline mb-4">OVERLINE</div>
+                        <v-list-item-title class="text-h5 mb-1">
+                            Headline 5
+                        </v-list-item-title>
+                        <v-list-item-subtitle
+                            >Greyhound divisely hello coldly
+                            fonwderfully</v-list-item-subtitle
+                        >
+                    </v-list-item-content>
+
+                    <v-list-item-avatar
+                        tile
+                        size="80"
+                        color="grey"
+                    ></v-list-item-avatar>
+                </v-list-item>
+
+                <v-card-actions>
+                    <v-btn outlined rounded text> Button </v-btn>
+                </v-card-actions>
+            </v-card>
+        </div>
+    </v-container>
 </template>
 
 <script>
-  export default {
+export default {
     name: 'HomePages',
-  }
+};
 </script>
