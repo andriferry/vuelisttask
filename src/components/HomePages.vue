23c2258880d3b7487c89b2ead4c7f746de3e0f36
andriferry <ferryandri35@gmail.com>
2022-03-26 09:09:40 +0700

    Update

--- src/components/HomePages.vue
+++ src/components/HomePages.vue
@@ -1,29 +1,8 @@
 <template>
     <v-container fluid>
-        <div class="d-flex" style="gap: 1rem;">
-            <v-card v-for="data in 15" :key="data"  max-width="344" outlined>
-                <v-list-item three-line>
-                    <v-list-item-content>
-                        <div class="text-overline mb-4">OVERLINE</div>
-                        <v-list-item-title class="text-h5 mb-1">
-                            Headline 5
-                        </v-list-item-title>
-                        <v-list-item-subtitle
-                            >Greyhound divisely hello coldly
-                            fonwderfully</v-list-item-subtitle
-                        >
-                    </v-list-item-content>
-
-                    <v-list-item-avatar
-                        tile
-                        size="80"
-                        color="grey"
-                    ></v-list-item-avatar>
-                </v-list-item>
-
-                <v-card-actions>
-                    <v-btn outlined rounded text> Button </v-btn>
-                </v-card-actions>
+        <div class="d-flex" style="gap: 1rem">
+            <v-card width="344" rounded="lg">
+                <v-btn block> Add list </v-btn>
             </v-card>
         </div>
     </v-container>
