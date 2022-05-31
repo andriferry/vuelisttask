22893bb01a98027d74ebf96235938fe6cfdf3e77
andriferry <ferryandri35@gmail.com>
2022-05-31 15:02:56 +0700

    Update app link

--- src/views/App.vue
+++ src/views/App.vue
@@ -11,6 +11,17 @@
                     transition="scale-transition"
                 />
 
+                <p
+                    class="mb-0 text-capitalize pb-0 text-h2 font-weight-black white--text"
+                >
+                    Vue list-Todo
+                </p>
+                <p
+                    class="mb-0 pb-0 text-h6 text-capitalize font-weight-black white--text"
+                >
+                    Vuejs App for todo list
+                </p>
+
                 <v-btn depressed large rounded color="" link to="/app">
                     get started
                 </v-btn>
